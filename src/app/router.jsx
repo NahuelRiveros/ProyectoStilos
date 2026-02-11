6fd161ea517e84bb0de298891c275696fd18f3bb
NahuelRiveros <70718987+NahuelRiveros@users.noreply.github.com>
2026-02-11 02:59:23 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/router.jsx
@@ -0,0 +1,68 @@
+import { createBrowserRouter } from "react-router-dom";
+import AppLayout from "../components/layout/AppLayout";
+import ProtectedRoute from "../components/protected_route.jsx";
+
+
+import HomePage from "../components/home_page.jsx";
+import KioskPage from "../pages/kiosk_page.jsx";
+import LoginPage from "../pages/login_page.jsx";
+import RegisterAlumnoPage from "../pages/register_page.jsx";
+
+import RecaudacionMensual from "../pages/estadisticas/recaudaciones_mensual_page.jsx";
+import RecaudacionMesPage from "../pages/estadisticas/recaudacion_mes_page.jsx";
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <AppLayout>
+        <HomePage />
+      </AppLayout>
+    ),
+  },
+  {
+    path: "/kiosk",
+    element: (
+      <AppLayout>
+        <KioskPage />
+      </AppLayout>
+    ),
+  },
+  {
+    path: "/login",
+    element: (
+      <AppLayout>
+        <LoginPage />
+      </AppLayout>
+    ),
+  },
+  {
+    path: "/register",
+    element: (
+      <AppLayout>
+        <RegisterAlumnoPage />
+      </AppLayout>
+    ),
+  },
+  {
+    path: "/estadisticas/recaudaciones-mensual",
+    element: (
+      <AppLayout>
+        <ProtectedRoute roles={["admin"]}>
+          <RecaudacionMensual />
+        </ProtectedRoute>
+      </AppLayout>
+    ),
+  },
+  {
+    path: "/estadisticas/recaudaciones/:anio/:mes",
+    element: (
+      <AppLayout>
+        <ProtectedRoute>
+          <RecaudacionMesPage />
+        </ProtectedRoute>
+      </AppLayout>
+    ),
+  },
+]);
