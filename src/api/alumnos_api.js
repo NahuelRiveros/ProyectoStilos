61a19303564f67bab92acdf9604ef493a981fd70
Riveros Edgardo Nahuel <70718987+NahuelRiveros@users.noreply.github.com>
2026-06-03 09:45:17 -0300

    reset

--- src/api/alumnos_api.js
+++
@@ -1,6 +0,0 @@
-import { http } from "./http";
-
-export async function registrarAlumno(payload) {
-  const r = await http.post("/personas/registrar", payload);
-  return r.data;
-}
--- src/api/kiosk_api.js
+++
@@ -1,6 +0,0 @@
-import { http } from "./http";
-
-export async function kioskIngreso(dni) {
-  const r = await http.post("/ingresos/registrar", { dni });
-  return r.data;
-}
